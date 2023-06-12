f5e4a23e4f5c4c5493c0a23306bf24ef1e50963e
Akbar Maliki Haqoni Jati <akbar.maliki@ui.ac.id>
2023-06-12 13:20:24 +0700

    Add Joke model

---
+++ Models/Joke.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+
+        public string QuestionAnswer { get; set; }
+
+        public Joke() { }
+    }
+}
